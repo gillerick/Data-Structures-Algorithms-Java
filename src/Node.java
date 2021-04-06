9626dd450c480caa7fe5db56a5b5c9f9720979f3
gillerick <=>
2021-04-06 20:34:11 +0300

    Added deleteNode method

--- src/Node.java
+++ src/Node.java
@@ -2,16 +2,33 @@ public class Node {
     Node next = null;
     int data;
 
-    public Node(int d){
+    public Node(int d) {
         data = d;
     }
 
-    void appendToTail(int d){
+    void appendToTail(int d) {
         Node end = new Node(d);
         Node n = this;
-        while (n.next != null){
+        while (n.next != null) {
             n = n.next;
         }
         n.next = end;
+    }
+
+    Node deleteNode(Node head, int d) {
+        Node n = head;
+
+        if (n.data == d) {
+            return head.next; /* Moved head*/
+        }
+
+        while (n.next != null) {
+            if (n.next.data == d) {
+                n.next = n.next.next;
+                return head;
+            }
+            n = n.next;
+        }
+        return head;
     }
 }
