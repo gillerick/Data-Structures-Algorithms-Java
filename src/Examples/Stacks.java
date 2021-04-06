b2b755af39c6588ebc230676c0f23555477cc05b
gillerick <=>
2021-04-06 21:34:46 +0300

    Added pop and push methods

--- src/Examples/Stacks.java
+++ src/Examples/Stacks.java
@@ -1,5 +1,7 @@
 package Examples;
 
+import java.util.EmptyStackException;
+
 public class Stacks<T> {
     private static class StackNode<T>{
         private T data;
@@ -11,4 +13,20 @@ public class Stacks<T> {
     }
 
     private StackNode<T> top;
+
+    //Pop method
+    public T pop(){
+        if (top == null) throw new EmptyStackException();
+        T item = top.data;
+        top = top.next;
+        return item;
+    }
+
+    public void push(T item){
+        StackNode<T> t = new StackNode<T>(item);
+        t.next = top;
+        top = t;
+    }
+
+
 }
